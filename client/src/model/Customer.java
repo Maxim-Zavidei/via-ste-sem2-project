405ed9f828629a7a0a3491d7f8dbf8fa3b3b9d31
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2021-04-21 20:24:12 +0200

    Added tiny stylization.

--- client/src/model/Customer.java
+++ client/src/model/Customer.java
@@ -1,14 +1,14 @@
 package model;
 
 public class Customer {
+
     private String name;
     private String surname;
     private int age;
     private MyDate birthday;
     private String gender;
 
-    public Customer(String name, String surname, int age, MyDate birthday, String gender)
-    {
+    public Customer(String name, String surname, int age, MyDate birthday, String gender) {
         this.name = name;
         this.surname = surname;
         this.age =age;
@@ -23,9 +23,9 @@ public class Customer {
     public String getSurname() {
         return surname;
     }
-    public String getFullName()
-    {
-        return name+" "+surname;
+
+    public String getFullName() {
+        return name + " " + surname;
     }
 
     public int getAge() {
@@ -40,20 +40,15 @@ public class Customer {
         return gender;
     }
 
-    public boolean equals(Object obj)
-    {
-        if (!(obj instanceof Customer))
-        {
-            return false;
-        }
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Customer)) return false;
         Customer other = (Customer) obj;
         return name.equals(other.name) && surname.equals(other.surname) && age == other.age && birthday.equals(other.birthday) && gender.equals(other.gender);
     }
 
-    public String toString()
-    {
-        String s ="";
-        s = getFullName()+" "+age+" "+birthday+" "+gender;
-        return s;
+    @Override
+    public String toString() {
+        return getFullName() + " " + age + " " + birthday + " " + gender;
     }
 }
