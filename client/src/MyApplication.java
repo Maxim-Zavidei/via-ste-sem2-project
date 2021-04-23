587272e5538c8770ec6e8366b7f7b96edfc5669c
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2021-04-23 21:27:02 +0200

    Default implementation of MyApplication class.

--- client/src/MyApplication.java
+++ client/src/MyApplication.java
@@ -1,10 +1,22 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
+import model.Model;
+import model.ModelManager;
+import view.ViewHandler;
+import viewmodel.ViewModelFactory;
 
 public class MyApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-
+        Model model = new ModelManager();
+        ViewModelFactory viewModelFactory = new ViewModelFactory(model);
+        ViewHandler viewHandler = new ViewHandler(viewModelFactory);
+        try {
+            viewHandler.start(primaryStage);
+        } catch (Exception e) {
+            System.err.println("Couldn't start the application.");
+            System.err.println(e.getMessage());
+        }
     }
 }
