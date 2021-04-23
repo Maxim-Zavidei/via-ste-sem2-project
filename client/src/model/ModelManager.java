d70349ac079b16a4ec85a8e841f3804803cd13cc
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2021-04-23 21:26:40 +0200

    Implemented a placeholder ModelManager class.

--- client/src/model/ModelManager.java
+++ client/src/model/ModelManager.java
@@ -1,5 +1,18 @@
 package model;
 
+import common.model.Product;
+import java.util.ArrayList;
+
 public class ModelManager implements Model {
-    
+
+    @Override
+    public ArrayList<Product> getCatalogOfProducts() {
+        // Temporary dummy data.
+        ArrayList<Product> toReturn = new ArrayList<>();
+        toReturn.add(new Product("1", 4, "Pain au Chocolate", "nice", 5));
+        toReturn.add(new Product("2", 1, "Golden Apple", "extra nice", 7.4));
+        toReturn.add(new Product("3", 3, "Sugar Bombs", "niche", 3.2));
+        toReturn.add(new Product("4", 7, "2 kg of Sweets", "niche extra", 1));
+        return toReturn;
+    }
 }
