8ea77e8012001a8e8def6ce97aa2ac231b172677
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2021-05-13 22:52:01 +0200

    Reformatted the code, updated for the User class changes.

--- common/src/common/model/Customer.java
+++ common/src/common/model/Customer.java
@@ -1,53 +1,69 @@
 package common.model;
 
-public class Customer extends User{
+public class Customer extends User {
 
-   // private String name;
-   // private String surname;
-   // private int age;
-   // private DateTime birthday;
-   // private String gender;
-
-    /**Constructor calling the super constructor*/
-    public Customer(String username, String password, String email, String name, String surname, DateTime birthday, char gender) {
-        super(username, password,email, name, surname, birthday, gender );
-    }
-    public Customer(String firstName, String lastName, String email){
-        super(firstName, lastName, email);
+    // Constructor with extended number of defined values.
+    public Customer(String email, String password, String name, String surname, DateTime birthday, char gender) {
+        super(email, password, name, surname, birthday, gender);
     }
 
-    /**Getters calling the super getters*/
-    public String getUsername(){return super.getUsername();}
-    public String getPassword(){return super.getPassword();}
-    public String getFirstName() { return super.getFirstName(); }
-    public String getLastName() { return super.getLastName();}
-    public String getFullName() { return super.getFullName();}
-    public int getAge() { return super.getAge(); }
-    public DateTime getBirthday() { return super.getBirthday(); }
-    public char getGender() {return super.getGender();}
-
-    @Override String getStatus()
-    {
-        return "Customer";
+    // Constructor with minimal required values.
+    public Customer(String email, String password) {
+        super(email, password);
     }
 
-    /**Setters calling the super setters*/
-    public void setUsername(String username) { super.setUsername(username); }
-    public void setPassword(String password) { super.setPassword(password); }
-    public void setFirstName(String firstName) { super.setFirstName(firstName); }
-    public void setLastName(String lastName) { super.setLastName(lastName); }
-    public void setGender(char gender) {super.setGender(gender); }
+    /**
+     * Getters calling the super getters
+     */
+    public String getPassword() {
+        return super.getPassword();
+    }
 
-   /* @Override
-    public boolean equals(Object obj) {
-        if (!(obj instanceof Customer)) return false;
-        Customer other = (Customer) obj;
-        return name.equals(other.name) && surname.equals(other.surname) && age == other.age && birthday.equals(other.birthday) && gender.equals(other.gender);
-    }*/
+    public String getFirstName() {
+        return super.getFirstName();
+    }
+
+    public String getLastName() {
+        return super.getLastName();
+    }
+
+    public String getFullName() {
+        return super.getFullName();
+    }
+
+    public int getAge() {
+        return super.getAge();
+    }
+
+    public DateTime getBirthday() {
+        return super.getBirthday();
+    }
+
+    public char getGender() {
+        return super.getGender();
+    }
+
+    /**
+     * Setters calling the super setters
+     */
+    public void setPassword(String password) {
+        super.setPassword(password);
+    }
+
+    public void setFirstName(String firstName) {
+        super.setFirstName(firstName);
+    }
+
+    public void setLastName(String lastName) {
+        super.setLastName(lastName);
+    }
+
+    public void setGender(char gender) {
+        super.setGender(gender);
+    }
 
     @Override
     public String toString() {
         return super.toString();
     }
-
 }
