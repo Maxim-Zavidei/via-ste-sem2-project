f986511a0c09437620ca092f66ae06e0d65146f3
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2021-05-14 14:45:10 +0200

    Implemented a client Model interface holding placeholder methods for authentication functionality.

--- client/src/model/Model.java
+++ client/src/model/Model.java
@@ -1,13 +1,23 @@
 package model;
 
-import common.model.UserManagement;
 import common.model.Product;
-import common.utility.observer.javaobserver.NamedPropertyChangeSubject;
-
 import java.util.ArrayList;
 
-public interface Model extends NamedPropertyChangeSubject {
-    ArrayList<Product> getCatalogOfProducts();
-    UserManagement getManagement();
-     void addProduct(Product product);
+public interface Model {
+    void stop();
+    void authenticate(String email, String password) throws Exception;
+    boolean deauthenticate();
+    ArrayList<Product> getCatalogOfProducts() throws Exception;
+
+
+//    void addProduct(Product product) throws Exception;
+//    void removeProduct(Product product) throws Exception;
+//    //void removeProduct(String name) throws Exception;
+//    //Product getProduct(String name) throws Exception;
+//    void addUser(User user) throws Exception;
+//    void removeUser(User user) throws Exception;
+//    //void removeUser(String email) throws Exception;
+//    //User getUser(String email) throws Exception;
+//    //User getUser(User user) throws Exception;
+//    //User getUser(int index throws Exception);
 }
