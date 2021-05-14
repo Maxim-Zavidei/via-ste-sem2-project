6a79c229efb9f15e39909128eb8194f0b7678038
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2021-05-14 13:53:32 +0200

    Added all necessary methods for the authentication and shopping window.

--- common/src/common/network/RemoteServerInterface.java
+++ common/src/common/network/RemoteServerInterface.java
@@ -1,4 +1,12 @@
 package common.network;
 
-public interface RemoteServerInterface {
+import common.model.Product;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+public interface RemoteServerInterface extends Remote {
+    RemoteServerInterface authenticate(RemoteClientInterface client, String email, String password) throws RemoteException;
+    void deauthenticate(RemoteClientInterface client) throws RemoteException;
+    ArrayList<Product> getCatalogOfProducts() throws RemoteException;
 }
