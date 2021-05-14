34b11480eba743414d2726b71d08fc3178b43b34
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2021-05-14 13:40:06 +0200

    Adapted the EmployeeList class under the changes of the Employee class.

--- common/src/common/model/EmployeeList.java
+++ common/src/common/model/EmployeeList.java
@@ -3,54 +3,48 @@ package common.model;
 import java.util.HashMap;
 import java.util.Map;
 
-public class EmployeeList
-{
-  /**Instance variables*/
-  private Map<String, Employee> employees;
-  /************************************/
+public class EmployeeList {
+    /**
+     * Instance variables
+     */
+    private Map<String, Employee> employees;
 
-  /**Constructor*/
-  public EmployeeList(){
-    employees = new HashMap<>();
-    dummyData();
-  }
-
-  private void dummyData()
-  {
-    Employee employee1 = new Employee("Stefan", "Georgiev");
-    employees.put(employee1.getUsername(), employee1);
-    Employee employee2 = new Employee("Maxim", "Zavidei", 'M');
-    employees.put(employee2.getUsername(), employee2);
-    Employee employee3 = new Employee("Yoana", "Miteva");
-    employees.put(employee3.getUsername(), employee3);
-    Employee employee4 = new Employee("Valeriu", "Rosca");
-    employees.put(employee4.getUsername(), employee4);
-  }
-  /****************************/
-
-  /**Methods*/
-  public void addEmployee(Employee employee){
-    employees.put(employee.getUsername(), employee);
-  }
-  public void removeEmployee(Employee employee){
-    employees.remove(employee.getUsername(), employee);
-  }
-  public void removeEmployee(String name){
-    employees.remove(name);
-  }
-  public Employee getEmployee(String name){
-    return employees.get(name);
-  }
-  public Employee getEmployee(int index){
-    return employees.get(index);
-  }
-  public int getSize(){return employees.size();}
-  public String getEmployees(){
-    StringBuilder returnEmployees= new StringBuilder();
-    for (Map.Entry<String, Employee> employee : employees.entrySet()){
-      returnEmployees.append(employee.getValue().toString());
+    /**
+     * Constructor
+     */
+    public EmployeeList() {
+        employees = new HashMap<>();
     }
-    return returnEmployees.toString();
-  }
-  /**************************************/
+
+
+    /**
+     * Methods
+     */
+    public void addEmployee(Employee employee) {
+        employees.put(employee.getEmail(), employee);
+    }
+
+    public void removeEmployee(Employee employee) {
+        employees.remove(employee.getEmail());
+    }
+
+    public void removeEmployee(String email) {
+        employees.remove(email);
+    }
+
+    public Employee getEmployee(String email) {
+        return employees.get(email);
+    }
+
+    public int getSize() {
+        return employees.size();
+    }
+
+    public String getEmployees() {
+        StringBuilder returnEmployees = new StringBuilder();
+        for (Map.Entry<String, Employee> employee : employees.entrySet()) {
+            returnEmployees.append(employee.getValue().toString());
+        }
+        return returnEmployees.toString();
+    }
 }
