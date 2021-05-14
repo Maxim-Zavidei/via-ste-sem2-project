fa9b727797d97e6dac394138fafb1747fb48b11c
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2021-05-14 13:25:44 +0200

    Modified the class to start up the server connection along with the model.

--- server/src/MyApplication.java
+++ server/src/MyApplication.java
@@ -1,10 +1,19 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
+import mediator.Server;
+import model.Model;
+import model.ModelManager;
 
 public class MyApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-
+        Model model = new ModelManager();
+        try {
+            Server server = new Server(model);
+        } catch (Exception e) {
+            System.err.println("Could not start the server.");
+            System.err.println(e.getMessage());
+        }
     }
 }
