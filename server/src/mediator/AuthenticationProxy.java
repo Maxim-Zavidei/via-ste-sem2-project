8756af4049f0b69e9aa65f45559d2f3c6e8e0f9a
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2021-05-14 14:19:14 +0200

    Implemented the AuthenticationProxy with functionality to authenticate and deauthenticate for now.

---
+++ server/src/mediator/AuthenticationProxy.java
@@ -0,0 +1,59 @@
+package mediator;
+
+import common.model.Product;
+import common.model.User;
+import common.model.UserList;
+import common.network.RemoteClientInterface;
+import common.network.RemoteServerInterface;
+import common.utility.collection.BidirectionalHashMap;
+import model.Model;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+
+public class AuthenticationProxy implements RemoteServerInterface {
+
+    private Model model;
+    private UserList cache;
+    private BidirectionalHashMap<RemoteClientInterface, GenericAccessType> authenticatedInstances;
+
+    public AuthenticationProxy(Model model) throws Exception {
+        this.model = model;
+        cache = model.getAllRegisteredUsers();
+        authenticatedInstances = new BidirectionalHashMap<>();
+        UnicastRemoteObject.exportObject(this, 0);
+        Naming.rebind("access", this);
+    }
+
+    @Override
+    public RemoteServerInterface authenticate(RemoteClientInterface client, String email, String password) throws RemoteException {
+        // Validate the provided arguments.
+        if (email == null || email.isEmpty()) throw new IllegalArgumentException("Email value can't be null or empty.");
+        if (password == null || password.isEmpty()) throw new IllegalArgumentException("Password value can't be null or empty.");
+        // Check if such a user is registered in the system.
+        User tmp = cache.getUser(email);
+        if (tmp == null || !tmp.getPassword().equals(password)) throw new IllegalArgumentException("Invalid email or password.");
+        // Checks if the given user is already logged in.
+        GenericAccessType toReturn;
+        try {
+            toReturn = tmp.isEmployee() ? new EmployeeAuthenticated(model, email, password) : new CustomerAuthenticated(model, email, password);
+        } catch (Exception e) {
+            throw new IllegalStateException("Could not authenticate at this moment, try later.");
+        }
+        if (authenticatedInstances.getKey(toReturn) != null) throw new IllegalStateException("This user is already authenticated.");
+        // Return the real subject to be replaced instead of the proxy on the client side.
+        authenticatedInstances.put(client, toReturn);
+        return toReturn;
+    }
+
+    @Override
+    public void deauthenticate(RemoteClientInterface client) {
+        if (authenticatedInstances.getValue(client) != null) authenticatedInstances.remove(client);
+    }
+
+    @Override
+    public ArrayList<Product> getCatalogOfProducts() throws RemoteException {
+        throw new IllegalStateException("Authenticate in order to perform this request.");
+    }
+}
