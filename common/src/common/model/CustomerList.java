6583493849f37eaef67c7cb6ef24e70e22d2e517
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2021-05-14 13:36:30 +0200

    Adapted the CustomerList class under the changes of the Customer class.

--- common/src/common/model/CustomerList.java
+++ common/src/common/model/CustomerList.java
@@ -3,43 +3,51 @@ package common.model;
 import java.util.HashMap;
 import java.util.Map;
 
-public class CustomerList
-{
-  /**Instance variables*/
-  private Map<String, Customer> customers;
-  /************************************/
+public class CustomerList {
+    /**
+     * Instance variables
+     */
+    private Map<String, Customer> customers;
 
-  /**Constructor*/
-  public CustomerList(){
-    customers = new HashMap<>();
-  }
-
-
-  /****************************/
-
-  /**Methods*/
-  public void addCustomer(Customer customer){
-    customers.put(customer.getUsername(), customer);
-  }
-  public void removeCustomer(Customer customer){
-    customers.remove(customer.getUsername(), customer);
-  }
-  public void removeCustomer(String name){
-    customers.remove(name);
-  }
-  public Customer getCustomer(String name){
-    return customers.get(name);
-  }
-  public Customer getCustomer(int index){
-    return customers.get(index);
-  }
-  public int getSize(){return customers.size();}
-  public String getCustomers(){
-    StringBuilder returnCustomers= new StringBuilder();
-    for (Map.Entry<String, Customer> customer : customers.entrySet()){
-      returnCustomers.append(customer.getValue().toString());
+    /**
+     * Constructor
+     */
+    public CustomerList() {
+        customers = new HashMap<>();
     }
-    return returnCustomers.toString();
-  }
-  /**************************************/
+
+    /**
+     * Methods
+     */
+    public void addCustomer(Customer customer) {
+        customers.put(customer.getEmail(), customer);
+    }
+
+    public void removeCustomer(Customer customer) {
+        customers.remove(customer.getEmail());
+    }
+
+    public void removeCustomer(String name) {
+        customers.remove(name);
+    }
+
+    public Customer getCustomer(String name) {
+        return customers.get(name);
+    }
+
+    public Customer getCustomer(int index) {
+        return customers.get(index);
+    }
+
+    public int getSize() {
+        return customers.size();
+    }
+
+    public String getCustomers() {
+        StringBuilder returnCustomers = new StringBuilder();
+        for (Map.Entry<String, Customer> customer : customers.entrySet()) {
+            returnCustomers.append(customer.getValue().toString());
+        }
+        return returnCustomers.toString();
+    }
 }
