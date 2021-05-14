5b4aa76eb34cf928b2178a53ae863cba1c0ec87c
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2021-05-14 13:20:03 +0200

    Reformatted and modified the Employee class to support the change of it's parent User class.

--- common/src/common/model/Employee.java
+++ common/src/common/model/Employee.java
@@ -1,82 +1,68 @@
 package common.model;
 
-public class Employee extends User
-{
-  /**Instance variables*/
- // private String firstName;
-  //private String lastName;
- // private String abbreviation;
- // private String email;
-  // ? private String username; ? part of super class user //
-  // ? private String password; ? part of super class user //
-  /***********************/
+public class Employee extends User {
 
-  /**Constructors*/
-  /*
-  * public Employee(String username, String password, String firstName, String lastName){
-  * super(username,password);
-  * this(firstName, lastName);
-  * } */
-  public Employee(){
-    super(createUsername("No", "Name"), "adm", "", "No", "Name", null, 'o');
-    //setAbbreviation();
-  }
-  public Employee(String firstName, String lastName){
-    super(createUsername(firstName,lastName), "adm","", firstName, lastName, null, 'o');
-  }
-  public Employee(String firstName, String lastName, char gender){
-    super(createUsername(firstName,lastName), "adm","", firstName, lastName, null, gender);
-  }
-  public Employee(String firstName, String lastName, String abbreviation, char gender){
-    super(abbreviation, "adm","", firstName, lastName, null, gender);
-  }
-  public Employee(String username, String password,String email)
-  {
-    super(username, password, email);
-  }
-  /**************************************************************************************/
+    // Constructor with extended number of defined values.
+    public Employee(String email, String password, String name, String surname, DateTime birthday, char gender) {
+        super(email, password, name, surname, birthday, gender);
+    }
 
-  /**Getters*/
-  public String getFirstName()
-  {
-    return super.getFirstName();
-  }
-  public String getLastName()
-  {
-    return super.getLastName();
-  }
-  public String getUsername()
-  {
-    return super.getUsername();
-  }
-  public String getFullName() { return super.getFullName();}
-  public int getAge() { return super.getAge(); }
-  public DateTime getBirthday() { return super.getBirthday(); }
-  public char getGender() {return super.getGender();}
-  /************************************************/
+    // Constructor with minimal required values.
+    public Employee(String email, String password) {
+        super(email, password);
+    }
 
-  /**Setters*/
-  public void setPassword(String password) { super.setPassword(password); }
-  public void setFirstName(String firstName) { super.setFirstName(firstName); }
-  public void setLastName(String lastName) { super.setLastName(lastName); }
-  public void setGender(char gender) {super.setGender(gender); }
+    /**
+     * Getters
+     */
+    public String getFirstName() {
+        return super.getFirstName();
+    }
 
-  @Override String getStatus(){
-    return "Employee";
-  }
-  /**************************************************************************************/
+    public String getLastName() {
+        return super.getLastName();
+    }
 
-  /**Methods*/
-  public String toString(){
-    return super.toString();
-  }
-  public boolean equals(Object obj){
-    if(!(obj instanceof Employee))return false;
-    Employee another = (Employee) obj;
-    return another.getFirstName().equals(getFirstName()) && another.getLastName().equals(getLastName());
-  }
-  public static String createUsername(String firstName, String lastName){
-    return firstName.charAt(0)+lastName.charAt(0)+"";
-  }
-  // * ^^ probably changed with super class methods
+    public String getFullName() {
+        return super.getFullName();
+    }
+
+    public int getAge() {
+        return super.getAge();
+    }
+
+    public DateTime getBirthday() {
+        return super.getBirthday();
+    }
+
+    public char getGender() {
+        return super.getGender();
+    }
+
+    /**
+     * Setters
+     */
+    public void setPassword(String password) {
+        super.setPassword(password);
+    }
+
+    public void setFirstName(String firstName) {
+        super.setFirstName(firstName);
+    }
+
+    public void setLastName(String lastName) {
+        super.setLastName(lastName);
+    }
+
+    public void setGender(char gender) {
+        super.setGender(gender);
+    }
+
+    /**
+     * Methods
+     */
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
