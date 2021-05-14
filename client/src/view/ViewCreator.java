d97d79af8ef07b475f722127da261367b9dbb6bb
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2021-05-14 13:12:54 +0200

    Added an informative message to go along with the error if the method happens to throw one.

--- client/src/view/ViewCreator.java
+++ client/src/view/ViewCreator.java
@@ -2,7 +2,6 @@ package view;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.Region;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,7 +9,7 @@ public abstract class ViewCreator {
 
     private Map<String, ViewController> allInstances = new HashMap<>();
 
-    public ViewController getViewController(View view) throws IOException {
+    public ViewController getViewController(View view) throws Exception {
         ViewController viewController = allInstances.get(view.getFxmlFile());
         if (viewController == null) {
             viewController = loadFromFXML(view.getFxmlFile());
@@ -23,12 +22,16 @@ public abstract class ViewCreator {
 
     protected abstract void initViewController(ViewController viewController, Region root);
 
-    private ViewController loadFromFXML(String fxmlFile) throws IOException {
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(ViewController.class.getResource(fxmlFile));
-        Region root = loader.load();
-        ViewController viewController = loader.getController();
-        initViewController(viewController, root);
-        return viewController;
+    private ViewController loadFromFXML(String fxmlFile) throws Exception {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(ViewController.class.getResource(fxmlFile));
+            Region root = loader.load();
+            ViewController viewController = loader.getController();
+            initViewController(viewController, root);
+            return viewController;
+        } catch (Exception e) {
+            throw new Exception("Could not load the fxml file: " + fxmlFile);
+        }
     }
 }
