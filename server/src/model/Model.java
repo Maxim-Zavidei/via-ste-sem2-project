b176e3dd01304deca56796ce7a7ddaaeb4037c01
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2021-05-17 18:59:21 +0200

    Renamed a method.

--- server/src/model/Model.java
+++ server/src/model/Model.java
@@ -9,8 +9,7 @@ import java.util.ArrayList;
 public interface Model {
     UserList getAllRegisteredUsers();
     void register(String email, String password, String firstName, String lastName, LocalDate birthday, char gender) throws IllegalArgumentException, IllegalStateException;
-    User getAuthenticatedUser(String email) throws IllegalStateException;
+    User getUser(String email) throws IllegalStateException;
     ArrayList<Product> getCatalogOfProducts();
-    void createDummyData();
     void addProduct(Product product) throws IllegalArgumentException, IllegalStateException;
 }
