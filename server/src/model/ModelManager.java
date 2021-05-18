2a824db8d995c37f3a95c35eb46f8292808e68ba
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2021-05-18 17:10:41 +0200

    Fixed proper error throwing in the server side data management methods.

--- server/src/model/ModelManager.java
+++ server/src/model/ModelManager.java
@@ -35,8 +35,7 @@ public class ModelManager implements Model {
     public UserList getAllRegisteredUsers() throws IllegalStateException {
         try {
             return userDAO.allUsers();
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (SQLException e) {
             throw new IllegalStateException("Server is unavailable at the moment. Try Later.");
         }
     }
@@ -50,7 +49,7 @@ public class ModelManager implements Model {
             if (userDAO.readByEmail(email) != null) throw new IllegalStateException("An user with this email is already registered.");
             // Store the newly registered user in the database.
             userDAO.create(toCreate);
-        } catch (Exception e) {
+        } catch (SQLException e) {
             throw new IllegalStateException("Server is unavailable at the moment. Try Later.");
         }
     }
@@ -62,7 +61,7 @@ public class ModelManager implements Model {
             User toReturn = userDAO.readByEmail(email);
             if (toReturn == null) throw new IllegalStateException("No registered user with such email could be found.");
             return toReturn;
-        } catch (Exception e) {
+        } catch (SQLException e) {
             throw new IllegalStateException("Server is unavailable at the moment. Try Later.");
         }
     }
@@ -73,17 +72,14 @@ public class ModelManager implements Model {
             User old = userDAO.readByEmail(oldEmail);
             // Checks if an user with this old email exists.
             if (old == null) throw new IllegalStateException("No registered user with such email could be found.");
-
             // Check if the new email is not already taken.
-            if (userDAO.readByEmail(newEmail) != null && !newEmail
-                .equals(oldEmail)) throw new IllegalStateException("The given new email is already taken.");
+            if (userDAO.readByEmail(newEmail) != null && !newEmail.equals(oldEmail)) throw new IllegalStateException("The given new email is already taken.");
             // Validate first the arguments through creating an object of type user.
             User current = isEmployee ? new Employee(newEmail, password, firstName, lastName, birthday, gender) : new Customer(newEmail, password, firstName, lastName, birthday, gender);
             // Update the newly registered user in the database and remove the old one.
             userDAO.delete(oldEmail);
             userDAO.create(current);
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
             throw new IllegalStateException("Server is unavailable at the moment. Try Later.");
         }
     }
@@ -94,7 +90,7 @@ public class ModelManager implements Model {
         try {
             if (userDAO.readByEmail(email) == null) throw new IllegalStateException("No registered user with such email could be found.");
             userDAO.delete(email);
-        } catch (Exception e) {
+        } catch (SQLException e) {
             throw new IllegalStateException("Server is unavailable at the moment. Try Later.");
         }
     }
@@ -103,7 +99,7 @@ public class ModelManager implements Model {
     public ArrayList<Product> getCatalogOfProducts() throws IllegalStateException {
         try {
             return new ArrayList<>(ProductDAOImpl.getInstance().read());
-        } catch (Exception e) {
+        } catch (SQLException e) {
             throw new IllegalStateException("Server is unavailable at the moment. Try Later.");
         }
     }
@@ -112,7 +108,7 @@ public class ModelManager implements Model {
     public void addProduct(Product product) throws IllegalStateException {
         try {
             productDAO.create(product.getQuantity(), product.getName(), product.getDescription(), product.getPrice());
-        } catch (Exception e) {
+        } catch (SQLException e) {
             throw new IllegalStateException("Server is unavailable at the moment. Try Later.");
         }
     }
@@ -122,7 +118,7 @@ public class ModelManager implements Model {
         try {
             if (productDAO.getById(product.getId()) == null) throw new IllegalStateException("No such product could be found.");
             productDAO.update(product);
-        } catch (Exception e) {
+        } catch (SQLException e) {
             throw new IllegalStateException("Server is unavailable at the moment. Try Later.");
         }
     }
@@ -132,7 +128,7 @@ public class ModelManager implements Model {
         try {
             if (productDAO.getById(product.getId()) == null) throw new IllegalStateException("No such product could be found.");
             productDAO.delete(product);
-        } catch (Exception e) {
+        } catch (SQLException e) {
             throw new IllegalStateException("Server is unavailable at the moment. Try Later.");
         }
     }
