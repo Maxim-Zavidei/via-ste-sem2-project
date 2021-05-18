788e6d5af2a374cb4f64e4d8b93cdd2a5f152df5
Valeriu1 <roscavaleriuvaleriu@gmail.com>
2021-05-18 18:38:50 +0200

    notification feature added to CatalogViewController

--- client/src/view/CatalogViewController.java
+++ client/src/view/CatalogViewController.java
@@ -1,12 +1,30 @@
 package view;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDialog;
+import com.jfoenix.controls.JFXDialogLayout;
+import com.jfoenix.controls.events.JFXDialogEvent;
 import javafx.collections.MapChangeListener;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.effect.BoxBlur;
+import javafx.scene.effect.ImageInput;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 import viewmodel.CatalogViewModel;
 import viewmodel.ProductViewModel;
 
+
+
 public class CatalogViewController extends ViewController {
+
+   @FXML public StackPane rootPane;
+   @FXML public BorderPane borderPane;
 
     private ViewHandler viewHandler;
     private CatalogViewModel viewModel;
@@ -26,6 +44,7 @@ public class CatalogViewController extends ViewController {
 
     @Override
     protected void init() {
+
         viewHandler = getViewHandler();
         viewModel = getViewModelFactory().getCatalogViewModel();
 
@@ -88,6 +107,7 @@ public class CatalogViewController extends ViewController {
 
     @FXML
     private void addToBasket() {
+        notificationProduct();
         viewModel.addToBasket();
     }
 
@@ -103,5 +123,44 @@ public class CatalogViewController extends ViewController {
         } catch (Exception e) {
             viewModel.getErrorProperty().set("!Could not logout at this time. Try later.");
         }
+    }
+
+    public void notificationProduct(){
+
+        //trying an image background
+        Image image = new Image("file:assets/cakeArt.png");
+
+        ImageInput imageInput = new ImageInput();
+        imageInput.setX(250);
+        imageInput.setY(100);
+        imageInput.setSource(image);
+
+        //blur.setInput(imageInput);
+
+        BoxBlur blur = new BoxBlur(3, 3, 3);
+
+        JFXDialogLayout dialogLayout = new JFXDialogLayout();
+        JFXButton button = new JFXButton("Okay");
+        JFXDialog dialog = new JFXDialog(rootPane,dialogLayout,JFXDialog.DialogTransition.CENTER);
+        button.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> dialog.close());
+
+        //unprofessional styling, but it works
+        button.setStyle("-fx-background-color: #461d5e; -fx-text-fill: white;");
+        dialog.setStyle("-fx-background-color: #ffb1b1cc;");
+        dialogLayout.setStyle("-fx-background-color: #ffb1b1cc;");
+
+        dialogLayout.setHeading(new Label("Check out the new cakes, NOW!"));
+        dialogLayout.setActions(button);
+        dialog.show();
+        dialog.setOnDialogClosed((JFXDialogEvent event) ->{
+            borderPane.setEffect(null);
+
+        });
+
+
+
+        borderPane.setEffect(blur);
+
+
     }
 }
