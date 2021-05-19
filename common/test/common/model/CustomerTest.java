c9ff123b221d054b9373a510fe49ef704aeaa65d
YoyoMy <35624284+YoyoMy@users.noreply.github.com>
2021-05-19 16:14:17 +0200

    Product test - shows needs for better validation in Product class

--- common/test/common/model/CustomerTest.java
+++ common/test/common/model/CustomerTest.java
@@ -272,4 +272,4 @@ class CustomerTest {
 
     }
 
-    }
+}
--- common/test/common/model/ProductTest.java
+++ common/test/common/model/ProductTest.java
@@ -34,10 +34,116 @@ class ProductTest {
     /**Test setters*/
     @Test
     void setNull(){
+        /**ID*/
+        assertThrows(IllegalArgumentException.class, () -> product.setId(null));
+        assertThrows(IllegalArgumentException.class, () -> product.setId(""));
+        //assertThrows(IllegalArgumentException.class, () -> product.setId(" "));
+        /**Product ID needs better checks*/
+
         /**Quantity*/
+        //product.setQuantity(0);
+        //assertEquals(0, product.getQuantity());
+        /**Why the quantity can't be less than 1? if it is 0, then it is unavailable at the moment?*/
 
         /**Name*/
+        assertThrows(IllegalArgumentException.class, () -> product.setName(null));
+        assertThrows(IllegalArgumentException.class, () -> product.setName(""));
+        //assertThrows(IllegalArgumentException.class, () -> product.setName(" "));
+        /**Name needs better checks*/
+
         /**Description*/
+        product.setDescription(null);
+        assertEquals("", product.getDescription());
+        product.setDescription("");
+        assertEquals("", product.getDescription());
+        product.setDescription(" ");
+        assertEquals(" ", product.getDescription());
         /**Price*/
+        assertThrows(IllegalArgumentException.class, () -> product.setPrice(0));
+        assertThrows(IllegalArgumentException.class, () -> product.setPrice(0));
+        assertThrows(IllegalArgumentException.class, () -> product.setPrice(0));
+        /**So the price can be null but the quantity not?*/
+    }
+
+    @Test
+    void setOne(){
+        /**ID*/
+        product.setId("1");
+        assertEquals("1", product.getId());
+        //assertThrows(IllegalArgumentException.class, () -> product.setId("one"));
+        /**Product ID needs better checks*/
+
+        /**Quantity*/
+        product.setQuantity(1);
+        assertEquals(1, product.getQuantity());
+        /**Name*/
+        product.setName("n");
+        assertEquals("n", product.getName());
+        /**Description*/
+        product.setDescription("n");
+        assertEquals("n", product.getDescription());
+        /**Price*/
+        product.setPrice(1);
+        assertEquals(1, product.getPrice());
+    }
+
+    @Test
+    void setMany(){
+        /**ID*/
+        product.setId("123");
+        assertEquals("123", product.getId());
+        //assertThrows(IllegalArgumentException.class, () -> product.setId("one"));
+        /**Product ID needs better checks*/
+
+        /**Quantity*/
+        product.setQuantity(123);
+        assertEquals(123, product.getQuantity());
+        /**Name*/
+        product.setName("name");
+        assertEquals("name", product.getName());
+        /**Description*/
+        product.setDescription("nsdsfdgfhgjfxtfhdytrsewertghjk,kjhreghjhghjm");
+        assertEquals("nsdsfdgfhgjfxtfhdytrsewertghjk,kjhreghjhghjm", product.getDescription());
+        /**Price*/
+        product.setPrice(231);
+        assertEquals(231, product.getPrice());
+        product.setPrice(2.3);
+        assertEquals(2.3, product.getPrice());
+    }
+
+    @Test
+    void setBoundary(){
+        /**ID*/
+        //assertThrows(IllegalArgumentException.class, () -> product.setId("-1"));
+        //assertThrows(IllegalArgumentException.class, () -> product.setId("0"));
+        /**Product ID needs better checks*/
+
+        /**Quantity*/
+        assertThrows(IllegalArgumentException.class, () -> product.setQuantity(-1));
+        /**Name
+         * no more cases*/
+        /**Description
+         * no more cases*/
+        /**Price*/
+        assertThrows(IllegalArgumentException.class, () -> product.setPrice(-1));
+    }
+
+    @Test
+    void setExceptions(){
+        /**ID*/
+        //assertThrows(IllegalArgumentException.class, () -> product.setId("-1"));
+        //assertThrows(IllegalArgumentException.class, () -> product.setId("0"));
+        /**Product ID needs better checks*/
+
+        /**Quantity*/
+        assertThrows(IllegalArgumentException.class, () -> product.setQuantity(-1));
+        assertThrows(IllegalArgumentException.class, () -> product.setQuantity(-23));
+        /**Name
+         * no more cases*/
+        /**Description
+         * no more cases*/
+        /**Price*/
+        assertThrows(IllegalArgumentException.class, () -> product.setPrice(-1));
+        assertThrows(IllegalArgumentException.class, () -> product.setPrice(-26));
     }
 }
